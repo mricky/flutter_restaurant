9c1a415a898a8fbc01f8a9595e7c23798ef8889c
Mohammad <mricky.it@gmail.com>
2019-12-05 18:58:28 +0800

    change stateless to statefull widget

--- flutter_complete_guide/lib/main.dart
+++ flutter_complete_guide/lib/main.dart
@@ -1,23 +1,36 @@
 import 'package:flutter/material.dart';
 
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-
-  var question =[
-    'What\'s your favourite color?',
-    'What\'s your favourite animal?'
-  ];
-  void answerQuestion(){
-    print('answer');
+  class MyApp extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+      // TODO: implement createState
+      return _MyAppState();
+    }
   }
+
+  class _MyAppState extends State<MyApp> {
+    var _questionIndex = 0;
+    void answerQuestion(){
+      setState(() {
+           _questionIndex = _questionIndex + 1;
+      });
+   
+      print(_questionIndex);
+    }
   @override
   Widget build(BuildContext context) {
+     var questions =[
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter Appbar'),),
         body: Column(children: <Widget>[
-          Text('The Questions ?'),
+          Text(questions[_questionIndex]),
           RaisedButton(child: Text('Answer 1'),onPressed: answerQuestion,),
           RaisedButton(child: Text('Answer 1'),onPressed: ()=> 'anwer 3'),
           RaisedButton(child: Text('Answer 1'),onPressed: (){
@@ -27,4 +40,5 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+  
 }
