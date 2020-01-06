27013279aa73b8939b54833160a18345e76ceb10
Mohammad <mricky.it@gmail.com>
2020-01-06 10:42:26 +0700

    Finish Items List

--- flutter_restaurant/lib/main.dart
+++ flutter_restaurant/lib/main.dart
@@ -329,3 +329,93 @@ class SelectedTypeSection extends StatelessWidget {
     );
   }
 }
+
+class MenuItemsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Polular Dishes',
+            style: TextStyle(fontSize: 22.0, color: Colors.black54),                
+          ),
+          SizedBox(height: 16.0),
+          MenuItem(),
+          MenuItem()
+          
+        ],
+      ),
+      
+    );
+  }
+}
+class MenuItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            height: 100.0,
+            width: 100.0,
+            child: Image.network(
+              burgerImage,
+              fit: BoxFit.cover
+            ),
+          ),
+          SizedBox(
+            width: 16.0,
+          ),
+          Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    color: iconYellow,
+                    borderRadius: BorderRadius.circular(4.0)
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 4.0
+                    ),
+                    child: Row(children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        size: 15.0,
+                      ),
+                      Text('4.5')
+                      
+                    ],
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 8.0,
+                ),
+                Text(
+                  'Special Chiken Burger',
+                  style: TextStyle(fontWeight: FontWeight.w600),
+                ),
+                Container(
+                  width: 200.0,
+                  child: Text(
+                    'Chicken, Yougurt, Red chilli, Ginger paste, Carlic Paste, ....',
+                     style: TextStyle(color: Colors.grey)
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      
+    );
+  }
+}
